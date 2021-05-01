17a0dc4c635a8b18dd8108ccd8deb6294cb9f878
Luke Penner <lukepenner@hotmail.ca>
2021-04-30 20:11:36 -0700

    initialized database skeleton with seeds

---
+++ db/schema/02_listings.sql
@@ -0,0 +1,13 @@
+-- Drop and recreate Widgets table (Example)
+
+DROP TABLE IF EXISTS listings CASCADE;
+CREATE TABLE listings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  price INTEGER NOT NULL,
+  image_url VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  date_posted TIMESTAMP NOT NULL,
+  is_sold BOOLEAN NOT NULL DEFAULT FALSE
+);
---
+++ db/seeds/02_listings.sql
@@ -0,0 +1,4 @@
+-- listings table seeds here (Example)
+INSERT INTO listings (user_id, price, image_url, title, description, date_posted) VALUES 
+(1, 50000, 'https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2249&q=80', 'French Bulldog', 'Cream and white french bulldog, 2 months old. Potty trained. Very loving. Looking for his furever home!', '2021-01-01 00:00:01'),
+(2,20000,'https://images.dog.ceo/breeds/cotondetulear/IMG_20160830_202631573.jpg', 'best doggo for sale', 'this one is the best','2021-01-01 00:00:01')
