1bd0a70e078b612aad11a08ee217b3cf448a62fd
Courtney Amos <c.amos@shaw.ca>
2021-05-03 16:37:04 -0700

    allow home page to be viewed by users that are unauth

--- routes/home.js
+++ routes/home.js
@@ -3,26 +3,64 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+    const userId = req.session.user_id;
+    console.log('userID: ', userId);
 
-    return db
-      .query(`
-        SELECT *
-        FROM listings
-        ORDER BY date_posted DESC;
-      `)
-      .then((queryResults) => {
-        const listings = queryResults.rows;
-        res.render("index", {
-          listings: listings,
-          user: req.user
+    if (userId) {
+      return db
+        .query(`
+          SELECT *
+          FROM users
+          WHERE users.id = '${userId}';
+        `)
+        .then((userQuery) => {
+          const userRows = userQuery.rows[0];
+
+          return db.query(`
+            SELECT *
+            FROM listings
+            ORDER BY listings.id DESC;
+          `)
+            .then((queryResults) => {
+              const listings = queryResults.rows;
+              const user = userRows.id;
+
+              return res.render("index", {
+                listings: listings,
+                user: user
+              });
+            })
+            .catch((err) => {
+              return res.render("error", {
+                message: 'An error occured while retrieving the listings, please try again',
+                redirect: '/'
+              });
+            });
         });
-      })
-      .catch((err) => {
-        return res.render("error", {
-          message: 'An error occured while retrieving the listings, please try again',
-          redirect: '/'
+    }
+
+    if (!userId) {
+      return db
+        .query(`
+          SELECT *
+          FROM listings
+          ORDER BY listings.id DESC;
+       `)
+        .then((queryResults) => {
+          const listings = queryResults.rows;
+
+          return res.render("index", {
+            listings: listings,
+            user: req.user
+          });
+        })
+        .catch((err) => {
+          return res.render("error", {
+            message: 'An error occured while retrieving the listings, please try again',
+            redirect: '/'
+          });
         });
-      });
+    }
   });
 
   // get specific item
@@ -38,11 +76,13 @@ module.exports = (db) => {
         const listings = queryResults.rows[0];
         res.render("listing",{
           listings: listings,
-          user: req.user});
+          user: req.user
+        });
       })
       .catch((err) => {
         return res.render("error", {
-          message: 'An error occured while retrieving the listings, please try again', redirect: '/'
+          message: 'An error occured while retrieving the listing, please try again',
+          redirect: '/'
         });
       });
   });
