810ffba79271dd42969a79d19f9677df53700655
Courtney Amos <c.amos@shaw.ca>
2021-05-03 10:12:35 -0700

    update favourties to order by id in descending order

--- routes/favourites.js
+++ routes/favourites.js
@@ -3,12 +3,15 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+    const userId = req.user.id;
+
     db.query(`
     SELECT *
     FROM favourites
     JOIN listings ON listings.id = favourites.listing_id
     JOIN users ON favourites.user_id = users.id
-    WHERE favourites.user_id = ${req.user.id};
+    WHERE favourites.user_id = ${userId}
+    ORDER BY favourites.id DESC;
     `)
       .then(data => {
         const favourites = data.rows;
