9bfb7d0d1fa331729b6ac1e0b90f7abd2b6d3b04
Luke Penner <lukepenner@hotmail.ca>
2021-05-06 20:13:47 -0700

    added new seeds

--- db/seeds/02_listings.sql
+++ db/seeds/02_listings.sql
@@ -1,33 +1,51 @@
 -- listings table seeds here (Example)
 INSERT INTO listings (user_id, price, image_url, title, description, date_posted) VALUES
-(1, 50000, 'https://images.unsplash.com/photo-1520848315518-b991dd16a625?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2550&q=80', 'Brown & White Husky!', 'Beautiful female husky looking for her forever home! Amazing breed for a companion dog', '2021-05-01'),
+(1, 5000, 'https://images.unsplash.com/photo-1520848315518-b991dd16a625?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2550&q=80', 'Brown & White Husky!', 'Beautiful female husky looking for her forever home! Amazing breed for a companion dog', '2021-05-01'),
 
-(2, 20000, 'https://images.unsplash.com/photo-1488740304459-45c4277e7daf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80', 'Siamese Kitten', 'Last kitten of the litter. Good temper and well socialized.', '2021-04-30'),
+(2, 200, 'https://images.unsplash.com/photo-1488740304459-45c4277e7daf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80', 'Siamese Kitten', 'Last kitten of the litter. Good temper and well socialized.', '2021-04-30'),
 
-(3, 200000, 'https://images.unsplash.com/photo-1541689221361-ad95003448dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80', 'Micro Pig', 'Micro pigs are only of the smallest pig breeds, growing only until knee height.', '2021-04-28'),
+(3, 2000, 'https://images.unsplash.com/photo-1541689221361-ad95003448dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80', 'Micro Pig', 'Micro pigs are only of the smallest pig breeds, growing only until knee height.', '2021-04-28'),
 
-(1, 500000, 'https://images.unsplash.com/photo-1493536122405-24f67df95909?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80', 'Bengal Cat', 'Highly intelligent and friendly in nature. Will be 8 to 15 pounds full grown. Will make a great additon to any family!', '2021-04-27'),
+(1, 500, 'https://images.unsplash.com/photo-1493536122405-24f67df95909?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80', 'Bengal Cat', 'Highly intelligent and friendly in nature. Will be 8 to 15 pounds full grown. Will make a great additon to any family!', '2021-04-27'),
 
-(5, 250000, 'https://images.unsplash.com/photo-1607037053371-15713e7386e8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2555&q=80', 'Blue Heeler Puppy!', 'This adorable littl guy is looking for his forever home. Best suited for a high energy family - as he will have lots of energy.', '2021-04-30'),
+(5, 2500, 'https://images.unsplash.com/photo-1607037053371-15713e7386e8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2555&q=80', 'Blue Heeler Puppy!', 'This adorable littl guy is looking for his forever home. Best suited for a high energy family - as he will have lots of energy.', '2021-04-30'),
 
-(1, 150000, 'https://images.unsplash.com/photo-1571167143301-3e6eb056e8e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80', 'Doberman For Rehoming', 'Natural 2 year old female doberman needing to be rehomed. Her former owner could no longer care for her. Please reach out if you think you would be a good fit.', '2021-04-27'),
+(1, 1500, 'https://images.unsplash.com/photo-1571167143301-3e6eb056e8e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80', 'Doberman For Rehoming', 'Natural 2 year old female doberman needing to be rehomed. Her former owner could no longer care for her. Please reach out if you think you would be a good fit.', '2021-04-27'),
 
-(1, 300000, 'https://images.unsplash.com/photo-1525253086316-d0c936c814f8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80', 'Australian Sheppard Puppies!', 'Litter ready to be rehomed on June 15, 2021. All girls are spoken for. Only 2 males left.', '2021-04-15'),
+(1, 300, 'https://images.unsplash.com/photo-1525253086316-d0c936c814f8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80', 'Australian Sheppard Puppies!', 'Litter ready to be rehomed on June 15, 2021. All girls are spoken for. Only 2 males left.', '2021-04-15'),
 
-(8, 10000, 'https://images.unsplash.com/photo-1585130451336-b0ac5daa3495?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2018&q=80', 'Cute pupper', 'beautiful dog, very friendly.', '2021-05-01'),
+(8, 1000, 'https://images.unsplash.com/photo-1585130451336-b0ac5daa3495?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2018&q=80', 'Cute pupper', 'beautiful dog, very friendly.', '2021-05-01'),
 
-(9, 30000, 'https://images.saymedia-content.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTczOTgyMzc1NjE5ODY0NDQz/cute-pet-names.jpg', 'cute kitten for sale', 'beautiful kitten ready for a new home', '2021-04-20'),
+(9, 300, 'https://images.saymedia-content.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTczOTgyMzc1NjE5ODY0NDQz/cute-pet-names.jpg', 'cute kitten for sale', 'beautiful kitten ready for a new home', '2021-04-20'),
 
-(10, 200000, 'https://themysteriousworld.com/wp-content/uploads/2017/12/baby-hedgehog.jpg', 'baby hedgehog', 'adorable hedgehog looking for his forever family!', '2021-05-02'),
+(10, 2000, 'https://themysteriousworld.com/wp-content/uploads/2017/12/baby-hedgehog.jpg', 'baby hedgehog', 'adorable hedgehog looking for his forever family!', '2021-05-02'),
 
 (11, 1000, 'https://www.pixelstalk.net/wp-content/uploads/2016/03/Cute-Animal-Wallpapers-Background-free.jpg', 'Cat friend', 'Wonderful cat, fun and playful, loves to cuddle', '2021-04-30'),
 
-(12, 95000, 'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2019/03/rabbit-475261_0.jpg?h=c855054e&itok=lfjXk4-x', 'Best Bunny', 'Lively little bunny, loves to hop around and play outside', '2021-04-27'),
+(12, 950, 'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2019/03/rabbit-475261_0.jpg?h=c855054e&itok=lfjXk4-x', 'Best Bunny', 'Lively little bunny, loves to hop around and play outside', '2021-04-27'),
 
-(13, 81000, 'https://yourspca.org/wp-content/uploads/2017/11/photo1741.jpg', 'BABY BUNNY', 'omg its so cute just look at his lil face', '2021-05-03'),
+(13, 810, 'https://yourspca.org/wp-content/uploads/2017/11/photo1741.jpg', 'BABY BUNNY', 'omg its so cute just look at his lil face', '2021-05-03'),
 
-(14, 9900, 'https://s7d2.scene7.com/is/image/PetSmart/ARHERO-ASetUpGuideForNewTurtleParents-20160818?$AR0301$', 'turtle friend', 'turtle friend will follow you around and loves lettuce, what a great guy', '2021-05-01'),
+(14, 990, 'https://s7d2.scene7.com/is/image/PetSmart/ARHERO-ASetUpGuideForNewTurtleParents-20160818?$AR0301$', 'turtle friend', 'turtle friend will follow you around and loves lettuce, what a great guy', '2021-05-01'),
 
-(15, 3000000, 'https://www.beverlyhillsvets.com/blog/wp-content/uploads/2017/12/BevHills_iStock-120691488.jpg', 'lizardo iguano', 'a majestic lizard, will eat all the flies. doesnt like people that much but thats ok.', '2021-04-19'),
+(15, 3000, 'https://www.beverlyhillsvets.com/blog/wp-content/uploads/2017/12/BevHills_iStock-120691488.jpg', 'lizardo iguano', 'a majestic lizard, will eat all the flies. doesnt like people that much but thats ok.', '2021-04-19'),
 
-(15, 2000, 'https://cdn.travelpulse.com/images/99999999-9999-9999-9999-999999999999/6ad83d0c-fbaf-6cdf-5cfe-e3d6de6716dc/630x355.jpg', 'Pink fish', 'I dont know what type of fish this is, but its pretty.', '2021-03-15');
+(15, 2000, 'https://cdn.travelpulse.com/images/99999999-9999-9999-9999-999999999999/6ad83d0c-fbaf-6cdf-5cfe-e3d6de6716dc/630x355.jpg', 'Pink fish', 'I dont know what type of fish this is, but its pretty.', '2021-03-15'),
+
+-- new
+
+(1, 410, 'https://images.unsplash.com/photo-1493916665398-143bdeabe500?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80', 'Big Dog', 'Big dog kind eyes big floppy ears, please take good care of him', '2021-03-27'),
+
+(1, 100, 'https://images.unsplash.com/photo-1425082661705-1834bfd09dca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80', 'hammy the hamster', 'little hamster, sweet and funny, little feets go pat pat pat', '2021-03-21'),
+
+(1, 2200, 'https://images.unsplash.com/photo-1491930722919-a5fc9ad49a75?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', 'Good boy', 'the best pupper ive ever seen, 11/10', '2021-04-21'),
+
+(1, 9400, 'https://images.unsplash.com/flagged/photo-1557427705-88c1e5a11a52?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80', 'Floofy kitty', 'soooo much floof', '2021-05-01'),
+
+(1, 3100, 'https://images.unsplash.com/photo-1453227588063-bb302b62f50b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80', 'Old man for sale', 'old man, he would like to stay inside or go for a short walk, no excitement for him thank you very much', '2021-05-04'),
+
+(1, 4290, 'https://images.unsplash.com/photo-1524511751214-b0a384dd9afe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80', 'Beautiful red haired dog', 'Young dog looking for a new home, full of energy and loves to play.', '2021-05-02'),
+
+(1, 400, 'https://images.unsplash.com/photo-1494205577727-d32e58564756?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80', 'Sad chihuahua', 'Chihuahua for sale because hes too sad :(', '2021-05-03'),
+
+(1, 900, 'https://images.unsplash.com/photo-1607027921602-fab1dca87451?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', 'Precious', 'Georgeous white haired kitty in need of loving family, contact asap', '2021-05-06');
